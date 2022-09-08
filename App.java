f215a662c2093c6625a23b4c18d2e6aa6fb8c438
Sohaib <sohaibramzan27@gmail.com>
2022-09-08 11:13:52 +0500

    Self drive car using if else statement

---
+++ App.java
@@ -0,0 +1,11 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        //self drive car using if-else statement
+
+        boolean isGreen = true;
+
+        if(isGreen){
+            System.out.println("drive the car");
+        }
+    }
+}
